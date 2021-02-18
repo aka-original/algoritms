1647b276514a36882d2383ee99834cddff196d20
Mikhail Razgonov <mrazgonov@yandex.ru>
2021-02-18 23:44:53 +0400

    2 function_less4

--- less4.py
+++ less4.py
@@ -1,11 +1,19 @@
 #определить число простое или составное, если простое вывести True
 def is_number(x):
-    devision=2
-    while devision<x:
-        if x%devision==0:
+    divisor=2
+    while divisor<x:
+        if x%divisor==0:
             return False
-        devision+=1
+        divisor+=1
     return True
+def factorize_number(x):
+    divisor=2
+    while x>1:
+        if x%divisor==0:
+            x//=divisor
+            print(divisor)
+        else: divisor+=1
 
 x=int(input())
-print(is_number(x))
+#print(is_number(x))
+print(factorize_number(x))
